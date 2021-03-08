b11b6a605665d6a4c2130e9973adee7e2a68b679
rohit prasain <rohit1@gmail.com>
2021-03-08 14:41:29 +0545

    downloaded images

--- resources/views/welcomehome.blade.php
+++ resources/views/welcomehome.blade.php
@@ -7,7 +7,7 @@
         <div class="overlay">
         </div>
         <div class="mainimage">
-            <img src="{{url('/images/products/mainproduct.png')}}" alt="">
+            <img src="{{url('/images/mainproduct.png')}}" alt="">
         </div>
     </div>
 </div>
@@ -21,7 +21,7 @@
     <div class="popular-cards">
         <div class="card">
             <div class="card-image">
-                <img src="{{url('/images/products/jackets.jpg')}}" alt="" height="200rem" width="200rem">
+                <img src="{{url('/images/popular/jackets.jpg')}}" alt="" height="200rem" width="200rem">
             </div>
 
             <div class="card-details" style="margin-top: .5rem;">
@@ -30,7 +30,7 @@
         </div>
         <div class="card">
             <div class="card-image">
-                <img src="{{url('/images/products/longsleeves.jpg')}}" alt="" height="200rem" width="200rem">
+                <img src="{{url('/images/popular/longsleeves.jpg')}}" alt="" height="200rem" width="200rem">
             </div>
 
             <div class="card-details" style="margin-top: .5rem;">
@@ -40,7 +40,7 @@
         </div>
         <div class="card">
             <div class="card-image">
-                <img src="{{url('/images/products/yogadress.jpg')}}" alt="" height="200rem" width="200rem">
+                <img src="{{url('/images/popular/yogadress.jpg')}}" alt="" height="200rem" width="200rem">
             </div>
 
             <div class="card-details" style="margin-top: .5rem;">
@@ -50,7 +50,7 @@
         </div>
         <div class="card">
             <div class="card-image">
-                <img src="{{url('/images/products/babyhoodie.jpg')}}" alt="" height="200rem" width="200rem">
+                <img src="{{url('/images/popular/babyhoodie.jpg')}}" alt="" height="200rem" width="200rem">
             </div>
 
             <div class="card-details" style="margin-top: .5rem;">
@@ -60,44 +60,47 @@
         </div>
         <div class="card">
             <div class="card-image">
-                <img src="{{url('/images/products/summertshirt.jpg')}}" alt="" height="200rem" width="200rem">
+                <img src="{{url('/images/popular/panthertop.jpg')}}" alt="" height="200rem" width="200rem">
             </div>
 
             <div class="card-details" style="margin-top: .5rem;">
-                <p style="color:#008cba; font-size:1.5rem">Summer T-Shirt</p>
+                <p style="color:#008cba; font-size:1.5rem">Panther Top</p>
+            </div>
+           
+        </div>
+
+        <div class="card">
+            <div class="card-image">
+                <img src="{{url('/images/popular/babyboytop.jpg')}}" alt="" height="200rem" width="200rem">
+            </div>
+
+            <div class="card-details" style="margin-top: .5rem;">
+                <p style="color:#008cba; font-size:1.5rem">BabyBoy Top</p>
+            </div>
+           
+        </div>
+        
+        <div class="card">
+            <div class="card-image">
+                <img src="{{url('/images/popular/butterflysleeve.jpg')}}" alt="" height="200rem" width="200rem">
+            </div>
+
+            <div class="card-details" style="margin-top: .5rem;">
+                <p style="color:#008cba; font-size:1.5rem">Butterfly Sleeve</p>
             </div>
            
         </div>
         <div class="card">
             <div class="card-image">
-                <img src="{{url('/images/products/babyhoodie.jpg')}}" alt="" height="200rem" width="200rem">
+                <img src="{{url('/images/popular/trumpetlongsleeve.jpg')}}" alt="" height="200rem" width="200rem">
             </div>
 
             <div class="card-details" style="margin-top: .5rem;">
-                <p style="color:#008cba; font-size:1.5rem">Baby Hoodie</p>
-            </div>
-           
-        </div>
-        <div class="card">
-            <div class="card-image">
-                <img src="{{url('/images/products/jackets.jpg')}}" alt="" height="200rem" width="200rem">
-            </div>
-
-            <div class="card-details" style="margin-top: .5rem;">
-                <p style="color:#008cba; font-size:1.5rem">Jackets</p>
+                <p style="color:#008cba; font-size:1.5rem">Trumpet Sleeve</p>
             </div>
             
         </div>
-        <div class="card">
-            <div class="card-image">
-                <img src="{{url('/images/products/longsleeves.jpg')}}" alt="" height="200rem" width="200rem">
-            </div>
-
-            <div class="card-details" style="margin-top: .5rem;">
-                <p style="color:#008cba; font-size:1.5rem">LongSleeves</p>
-            </div>
-           
-        </div>
+       
     </div>
 </div>
 
